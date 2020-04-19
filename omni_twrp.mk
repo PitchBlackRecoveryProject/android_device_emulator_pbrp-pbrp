49b1bf7a1344b3ac62c5ed58a55dcf24ea945977
Manjot Sidhu <manjot.techie@gmail.com>
2020-04-19 10:44:42 +0530

    Update omni_twrp.mk

--- omni_twrp.mk
+++ omni_twrp.mk
@@ -5,7 +5,7 @@ PRODUCT_RELEASE_NAME := twrp
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
 # Inherit from our custom product configuration
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/pb/config/common.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/emulator/twrp/device.mk)
